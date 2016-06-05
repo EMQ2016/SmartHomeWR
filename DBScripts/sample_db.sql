4a1608317f98b4776decfd5d170a8d53172e016a
Fabian Meyer <rookfighter@outlook.de>
2016-06-05 19:56:28 +0200

    Change error message handling

--- DBScripts/sample_db.sql
+++ DBScripts/sample_db.sql
@@ -15,12 +15,14 @@ INSERT INTO `group` (`gid`, `groupname`, `type`) VALUES
 INSERT INTO `group2module` (`grp2modulid`, `gid`, `mid`) VALUES
 (1, 1, 1),
 (2, 1, 2),
-(3, 1, 3);
+(3, 1, 3),
+(4, 1, 4);
 
 INSERT INTO `module` (`mid`, `moduleID`, `moduleName`) VALUES
 (1, '1', 'Berechtigungsmanagement'),
 (2, '', 'Verbrauchsübersicht'),
-(3, '', 'Energiemanagement');
+(3, '', 'Energiemanagement'),
+(4, 'opm2  [mov2#opm2]', 'ModuleDefinitions');
 
 INSERT INTO `user` (`uid`, `userName`, `password`, `email`) VALUES
 (1, 'timo', 'timo', 'emailhahaha');
